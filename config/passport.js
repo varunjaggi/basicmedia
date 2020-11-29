c6872f8330a298e20228fb19ff52682eed9028ca
varunjaggi <48566633+varunjaggi@users.noreply.github.com>
2020-11-29 06:11:37 +0530

    Updating

--- config/passport.js
+++ config/passport.js
@@ -40,7 +40,6 @@ module.exports = function (passport) {
   passport.serializeUser((user, done) => {
     done(null, user.id)
   })
-
   passport.deserializeUser((id, done) => {
     User.findById(id, (err, user) => done(err, user))
   })
